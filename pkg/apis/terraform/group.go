771d41a78322311ab8c33cdf81bbd8d6d34fe0cc
Louis Garman <louisgarman@gmail.com>
2020-02-25 18:35:28 +0000

    Skaffolds for command ctrller

---
+++ pkg/apis/terraform/group.go
@@ -0,0 +1,6 @@
+// Package terraform contains terraform API versions.
+//
+// This file ensures Go source parsers acknowledge the terraform package
+// and any child packages. It can be removed if any other Go source files are
+// added to this package.
+package terraform
---
+++ pkg/apis/terraform/v1alpha1/command_types.go
@@ -0,0 +1,48 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// CommandSpec defines the desired state of Command
+type CommandSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+}
+
+// CommandStatus defines the observed state of Command
+type CommandStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// Command is the Schema for the commands API
+// +kubebuilder:subresource:status
+// +kubebuilder:resource:path=commands,scope=Namespaced
+type Command struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   CommandSpec   `json:"spec,omitempty"`
+	Status CommandStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// CommandList contains a list of Command
+type CommandList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Command `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Command{}, &CommandList{})
+}
