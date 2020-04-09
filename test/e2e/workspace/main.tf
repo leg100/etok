e06dbecffda93f74cac3f11ec746db32d873c890
Louis Garman <louisgarman@gmail.com>
2020-04-09 08:05:18 +0100

    example workspace creates bucket

--- test/e2e/workspace/main.tf
+++ test/e2e/workspace/main.tf
@@ -8,3 +8,12 @@ terraform {
     bucket  = "master-anagram-224816-tfstate"
   }
 }
+
+resource "random_id" "test" {
+  byte_length = 2
+}
+
+resource "google_storage_bucket" "test" {
+  name     = "master-anagram-224816-${random_id.test.hex}"
+  location = "EU"
+}
