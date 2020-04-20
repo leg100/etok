8716bf285cac36f39089262fa7ac2cb66391b799
Louis Garman <louisgarman@gmail.com>
2020-04-20 22:56:04 +0100

    Fall back to logs if attach fails

--- app/app.go
+++ app/app.go
@@ -136,7 +136,7 @@ func (app *App) Run() error {
 
 	// TODO: make timeout configurable
 	log.Debugln("Waiting for pod to be running and ready...")
-	_, err = app.WaitForPod(name, podRunningAndReady, 10*time.Second)
+	pod, err := app.WaitForPod(name, podRunningAndReady, 10*time.Second)
 	if err != nil {
 		return err
 	}
@@ -149,31 +149,40 @@ func (app *App) Run() error {
 	}
 	defer logs.Close()
 
+	done := make(chan error)
+	go func() {
+		log.Debugln("Attach to pod TTY...")
+		err := app.handleAttachPod(pod)
+		if err != nil {
+			log.Warn("Failed to attach to pod TTY; falling back to streaming logs")
+			_, err = io.Copy(os.Stdout, logs)
+			done <- err
+		} else {
+			done <- nil
+		}
+	}()
+
 	// let operator know we're now streaming logs
 	log.Debugln("Telling the operator I'm ready to receive logs...")
 	retry.RetryOnConflict(retry.DefaultRetry, func() error {
 		log.Debugln("Attempt to update command resource...")
 		key := types.NamespacedName{Name: name, Namespace: app.Namespace}
 		if err = app.Client.Get(context.Background(), key, command); err != nil {
-			panic(err.Error())
-		}
+			done <- err
+		} else {
+			annotations := command.GetAnnotations()
+			if annotations == nil {
+				annotations = make(map[string]string)
+			}
+			annotations["stok.goalspike.com/client"] = "Ready"
+			command.SetAnnotations(annotations)
 
-		annotations := command.GetAnnotations()
-		if annotations == nil {
-			annotations = make(map[string]string)
+			return app.Client.Update(context.Background(), command)
 		}
-		annotations["stok.goalspike.com/client"] = "Ready"
-		command.SetAnnotations(annotations)
-
-		return app.Client.Update(context.Background(), command)
+		return nil
 	})
 
-	log.Debugln("Copying logs to stdout...")
-	_, err = io.Copy(os.Stdout, logs)
-	if err != nil {
-		return err
-	}
-	return nil
+	return <-done
 }
 
 func (app *App) CheckWorkspaceExists() (bool, error) {
@@ -258,7 +267,7 @@ func (app *App) handleAttachPod(pod *corev1.Pod) error {
 			IOStreams: genericclioptions.IOStreams{
 				In:     os.Stdin,
 				Out:    os.Stdout,
-				ErrOut: os.Stderr,
+				ErrOut: log.New().WriterLevel(log.WarnLevel),
 			},
 		},
 		Attach:        &attach.DefaultRemoteAttach{},
