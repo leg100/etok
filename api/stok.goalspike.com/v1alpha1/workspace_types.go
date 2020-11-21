3a1f70cc901a104aa90acf2aae282f52ade33864
Louis Garman <louisgarman@gmail.com>
2020-11-21 21:14:04 +0000

    Re-order workspace type specs

--- api/stok.goalspike.com/v1alpha1/workspace_types.go
+++ api/stok.goalspike.com/v1alpha1/workspace_types.go
@@ -5,17 +5,98 @@ import (
 	"sort"
 	"strings"
 
-	"github.com/creasty/defaults"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+func init() {
+	SchemeBuilder.Register(&Workspace{}, &WorkspaceList{})
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// Workspace is the Schema for the workspaces API
+// +kubebuilder:subresource:status
+// +kubebuilder:resource:path=workspaces,scope=Namespaced
+// +kubebuilder:printcolumn:name="Queue",type="string",JSONPath=".status.queue"
+// +kubebuilder:printcolumn:name="Backend",type="string",JSONPath=".spec.backend.type"
+// +kubebuilder:printcolumn:name="Phase",type="string",JSONPath=".status.phase"
+// +genclient
+type Workspace struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   WorkspaceSpec   `json:"spec,omitempty"`
+	Status WorkspaceStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// WorkspaceList contains a list of Workspace
+type WorkspaceList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Workspace `json:"items"`
+}
+
+// WorkspaceSpec defines the desired state of Workspace
+type WorkspaceSpec struct {
+	// +kubebuilder:default=stok
+	SecretName string `json:"secretName,omitempty"`
+	// +kubebuilder:default=stok
+	ServiceAccountName string `json:"serviceAccountName,omitempty"`
+
+	Cache   WorkspaceCacheSpec `json:"cache,omitempty"`
+	Backend BackendSpec        `json:"backend"`
+	Debug   bool               `json:"debug,omitempty"`
+
+	AttachSpec `json:",inline"`
+}
 
 // WorkspaceSpec defines the desired state of Workspace's cache storage
 type WorkspaceCacheSpec struct {
 	StorageClass string `json:"storageClass,omitempty"`
 	Size         string `json:"size,omitempty" default:"1Gi"`
 }
+
+func (ws *Workspace) PodName() string {
+	return WorkspacePodName(ws.GetName())
+}
+
+func (ws *Workspace) PVCName() string {
+	return ws.GetName()
+}
+
+func WorkspacePodName(name string) string {
+	return "workspace-" + name
+}
+
+// Get/Set Debug functions
+func (ws *Workspace) GetDebug() bool      { return ws.Spec.Debug }
+func (ws *Workspace) SetDebug(debug bool) { ws.Spec.Debug = debug }
+
+// WorkspaceStatus defines the observed state of Workspace
+type WorkspaceStatus struct {
+	Queue []string       `json:"queue"`
+	Phase WorkspacePhase `json:"phase"`
+}
+
+type WorkspacePhase string
+
+const (
+	WorkspacePhaseInitializing WorkspacePhase = "initializing"
+	WorkspacePhaseReady        WorkspacePhase = "ready"
+	WorkspacePhaseError        WorkspacePhase = "error"
+	WorkspacePhaseUnknown      WorkspacePhase = "unknown"
+	WorkspacePhaseDeleting     WorkspacePhase = "deleting"
+
+	WorkspaceDefaultCacheSize = "1Gi"
+
+	WorkspaceDefaultSecretName         = "stok"
+	WorkspaceDefaultServiceAccountName = "stok"
+
+	BackendTypeFilename   = "backend.tf"
+	BackendConfigFilename = "backend.ini"
+)
 
 type BackendSpec struct {
 	// +kubebuilder:validation:Enum=local;remote;artifactory;azurerm;consul;cos;etcd;etcdv3;gcs;http;manta;oss;pg;s3;swift
@@ -49,97 +130,4 @@ func BackendConfig(cfg map[string]string) string {
 
 func BackendConfigMapName(workspace string) string {
 	return "workspace-" + workspace
-}
-
-func (ws *Workspace) PodName() string {
-	return WorkspacePodName(ws.GetName())
-}
-
-func (ws *Workspace) PVCName() string {
-	return ws.GetName()
-}
-
-func WorkspacePodName(name string) string {
-	return "workspace-" + name
-}
-
-// WorkspaceSpec defines the desired state of Workspace
-type WorkspaceSpec struct {
-	// +kubebuilder:default=stok
-	SecretName string `json:"secretName,omitempty"`
-	// +kubebuilder:default=stok
-	ServiceAccountName string `json:"serviceAccountName,omitempty"`
-
-	Cache   WorkspaceCacheSpec `json:"cache,omitempty"`
-	Backend BackendSpec        `json:"backend"`
-	Debug   bool               `json:"debug,omitempty"`
-
-	AttachSpec `json:",inline"`
-}
-
-func (w *WorkspaceSpec) SetDefaults() {
-	if defaults.CanUpdate(w.SecretName) {
-		w.SecretName = "stok"
-	}
-	if defaults.CanUpdate(w.ServiceAccountName) {
-		w.ServiceAccountName = "stok"
-	}
-}
-
-// Get/Set Debug functions
-func (ws *Workspace) GetDebug() bool      { return ws.Spec.Debug }
-func (ws *Workspace) SetDebug(debug bool) { ws.Spec.Debug = debug }
-
-// WorkspaceStatus defines the observed state of Workspace
-type WorkspaceStatus struct {
-	Queue []string       `json:"queue"`
-	Phase WorkspacePhase `json:"phase"`
-}
-
-type WorkspacePhase string
-
-const (
-	WorkspacePhaseInitializing WorkspacePhase = "initializing"
-	WorkspacePhaseReady        WorkspacePhase = "ready"
-	WorkspacePhaseError        WorkspacePhase = "error"
-	WorkspacePhaseUnknown      WorkspacePhase = "unknown"
-	WorkspacePhaseDeleting     WorkspacePhase = "deleting"
-
-	WorkspaceDefaultCacheSize = "1Gi"
-
-	WorkspaceDefaultSecretName         = "stok"
-	WorkspaceDefaultServiceAccountName = "stok"
-
-	BackendTypeFilename   = "backend.tf"
-	BackendConfigFilename = "backend.ini"
-)
-
-// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
-
-// Workspace is the Schema for the workspaces API
-// +kubebuilder:subresource:status
-// +kubebuilder:resource:path=workspaces,scope=Namespaced
-// +kubebuilder:printcolumn:name="Queue",type="string",JSONPath=".status.queue"
-// +kubebuilder:printcolumn:name="Backend",type="string",JSONPath=".spec.backend.type"
-// +kubebuilder:printcolumn:name="Phase",type="string",JSONPath=".status.phase"
-// +genclient
-type Workspace struct {
-	metav1.TypeMeta   `json:",inline"`
-	metav1.ObjectMeta `json:"metadata,omitempty"`
-
-	Spec   WorkspaceSpec   `json:"spec,omitempty"`
-	Status WorkspaceStatus `json:"status,omitempty"`
-}
-
-// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
-
-// WorkspaceList contains a list of Workspace
-type WorkspaceList struct {
-	metav1.TypeMeta `json:",inline"`
-	metav1.ListMeta `json:"metadata,omitempty"`
-	Items           []Workspace `json:"items"`
-}
-
-func init() {
-	SchemeBuilder.Register(&Workspace{}, &WorkspaceList{})
 }
