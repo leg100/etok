a90119bfcd4808d09f586b0b2241e87dc073e0c9
Louis Garman <louisgarman@gmail.com>
2021-04-09 13:02:08 +0100

    Clean up config pkg

--- config/config.go
+++ config/config.go
@@ -2,21 +2,18 @@ package config
 
 import (
 	"embed"
-	iofs "io/fs"
-
-	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
-	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+	"io/fs"
 )
 
 //go:embed crd/bases/*.yaml
 //go:embed operator/*.yaml
 var operatorResources embed.FS
 
-// Utility func for retrieving list of files from an embedded fs.
+// Retrieve operator k8s resources from embedded fs
 func GetOperatorResources() ([][]byte, error) {
 	var resources [][]byte
 
-	err := iofs.WalkDir(operatorResources, ".", func(path string, d iofs.DirEntry, err error) error {
+	err := fs.WalkDir(operatorResources, ".", func(path string, d fs.DirEntry, err error) error {
 		if err != nil {
 			return err
 		}
@@ -25,7 +22,7 @@ func GetOperatorResources() ([][]byte, error) {
 			return nil
 		}
 
-		res, err := iofs.ReadFile(operatorResources, path)
+		res, err := fs.ReadFile(operatorResources, path)
 		if err != nil {
 			return err
 		}
